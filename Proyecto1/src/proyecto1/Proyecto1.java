d77cd0a13fe5b82dee52e2929652117ec3486f92
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2024-10-18 12:27:09 -0400

    test 2

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -14,6 +14,7 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("");
         // TODO code application logic here
     }
     
