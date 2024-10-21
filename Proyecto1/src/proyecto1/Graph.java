ba6ee761d273a9c7adbf8ba1d3ff20e7cea63c82
Djinn01 <diegotamayorodriguez@gmail.com>
2024-10-21 16:32:38 -0400

    a

--- Proyecto1/src/proyecto1/Graph.java
+++ Proyecto1/src/proyecto1/Graph.java
@@ -9,31 +9,20 @@ package proyecto1;
  * @author Djinn
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Graph {
-    private final List<NodeGraph> nodes; 
-
-    public Graph(int vertices) {
-        nodes = new ArrayList<>(vertices);
-        for (int i = 0; i < vertices; i++) {
-            nodes.add(new NodeGraph(i)); 
+class Graph {
+    public boolean esNulo = true;
+    
+    // Singleton
+    private static Graph instancia = null;
+    
+    public static Graph ObtenerInstancia() {
+        if (instancia == null) {
+            instancia = new Graph();
         }
+        return instancia;
     }
-    public void addEdge(int source, int destination) {
-        NodeGraph sourceNode = nodes.get(source);
-        NodeGraph destinationNode = nodes.get(destination);
-        sourceNode.addNeighbor(destinationNode);
-        destinationNode.addNeighbor(sourceNode); 
-    }
-    public void display() {
-        for (NodeGraph node : nodes) {
-            System.out.print(node.getVertex() + ": ");
-            for (NodeGraph neighbor : node.getNeighbors()) {
-                System.out.print(neighbor.getVertex() + " ");
-            }
-            System.out.println();
-        }
-    }
+    
+    Lista<NodoGrafo> nodos;
+    int T;
+    String nombreSistemaDeTransporte;
 }
