11cb642246710c032b6a51dd8bbcd17d55803493
Massimo Gramcko <m.gramcko@correo.unimet.edu.ve>
2024-10-21 12:27:59 -0400

    se agrego codigo en el main de la interfaz

--- Proyecto1/src/proyecto1/Interfaz.java
+++ Proyecto1/src/proyecto1/Interfaz.java
@@ -646,8 +646,11 @@ public class Interfaz extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+    public static void main(String args[])throws IOException {
+        Grafo.ObtenerInstancia().nodos = new Lista<NodoGrafo>();
+        Grafo.ObtenerInstancia().esNulo = true;
+        Grafo.ObtenerInstancia().nombreSistemaDeTransporte = "";
+        
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
