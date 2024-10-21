f78ba3ef82f2ba64869a4db09f006ac4acf62191
Massimo Gramcko <m.gramcko@correo.unimet.edu.ve>
2024-10-20 23:26:59 -0400

    Arreglo de NodoGrafo y Grafo

---
+++ Proyecto1/src/proyecto1/NodoGrafo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1;
+
+/**
+ *
+ * @author massi
+ */
+public class NodoGrafo {
+    
+            private Lista<NodoGrafo> vecinos;
+            private String nombreEstacion;
+            private String nombreDeLinea;
+
+            public NodoGrafo(Lista<NodoGrafo> vecinos, String nombreEstacion, String nombreDeLinea) {
+                this.vecinos = vecinos;
+                this.nombreEstacion = nombreEstacion;
+                this.nombreDeLinea = nombreDeLinea;
+            }
+
+            public Lista<NodoGrafo> getVecinos() {
+                return vecinos;
+            }
+
+            public void setVecinos(Lista<NodoGrafo> vecinos) {
+                this.vecinos = vecinos;
+            }
+
+            public String getNombreEstacion() {
+                return nombreEstacion;
+            }
+
+            public void setNombreEstacion(String nombreEstacion) {
+                this.nombreEstacion = nombreEstacion;
+            }
+
+            public String getNombreDeLinea() {
+                return nombreDeLinea;
+            }
+
+            public void setNombreDeLinea(String nombreDeLinea) {
+                this.nombreDeLinea = nombreDeLinea;
+            }
+}
