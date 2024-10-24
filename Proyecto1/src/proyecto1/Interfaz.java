e2120acb952adcd92b862c8bc1b2f20d22630d27
Andreapd3 <andrea.pareles@correo.unimet.edu.ve>
2024-10-24 08:36:29 -0700

    Correción text interfaz

--- Proyecto1/src/proyecto1/Interfaz.java
+++ Proyecto1/src/proyecto1/Interfaz.java
@@ -771,7 +771,7 @@ public class Interfaz extends javax.swing.JFrame {
 
         TextAbout.setColumns(20);
         TextAbout.setRows(5);
-        TextAbout.setText("Una reconocida cadena de supermercados busca entrar en Sudamérica, pero necesita un programa\n que le ayude a determinar la mejor ubicación de sus sucursales. Como política estratégica de\n la empresa, \nuna sucursal se ubicará dentro de un radio de no más de 100 metros de una parada del sistema \nde transporte urbano de la ciudad. Además, la Dirección de Operaciones de la empresa \nha generado un modelo geográfico de uso interno en el que cada parada se considera como el \ncentro geográfico alrededor del cual y en un radio equivalente a máximo t paradas, se define una zona \ncomercial. Cada zona es denominada como la parada elegida como su centro y que está próxima a la \nsucursal. Se considera que una sucursal cubre comercialmente una zona si la parada (centro de la zona) \nestá a no más de t paradas de una parada con sucursal (donde t es un número que puede variar por\n ciudad).\nEl programa debe permitirle al usuario seleccionar paradas para colocar sucursales y luego indicar \nla cobertura comercial, según la configuración creada. De esta forma, el programa ayudará al usuario a\n determinar si se logra cubrir las necesidades de la población que hace vida, al menos alrededor de las\n rutas de transporte urbano. La meta es cubrir enteramente la ciudad.\n");
+        TextAbout.setText("Modelo Geográfico\nCada parada de transporte se considera el centro de una zona comercial, dentro de un radio de hasta t paradas. Una sucursal cubre una zona si está a no más de t paradas de una parada con sucursal.\n\nFuncionalidades del Programa\nCargar Red de Transporte: Permite cargar y reemplazar redes de transporte desde archivos.\n\nMostrar Grafo: Visualiza la red de transporte usando GraphStream.\n\nEstablecer Valor de t: Posibilidad de cambiar el valor de t en cualquier momento para evaluar diferentes escenarios.\n\nColocar Sucursal: Seleccionar paradas para ubicar sucursales y ver la cobertura comercial.\n\nVer Cobertura de Sucursal: Usar DFS o BFS para mostrar paradas alcanzables desde una sucursal.\n\nRevisar Cobertura Total: Evaluar si las sucursales cubren toda la ciudad y sugerir nuevas ubicaciones si es necesario.\n\nAgregar Línea: Añadir nuevas líneas a la red para evaluar la cobertura futura.\n");
         jScrollPane1.setViewportView(TextAbout);
 
         javax.swing.GroupLayout Pagina4Layout = new javax.swing.GroupLayout(Pagina4);
@@ -1366,3 +1366,4 @@ public class Interfaz extends javax.swing.JFrame {
     private javax.swing.JPanel tab4;
     // End of variables declaration//GEN-END:variables
 }
+
